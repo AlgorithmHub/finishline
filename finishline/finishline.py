e856da75b12514d46ddebf0d3ab8a2faa6280c0a
Mobile Space Lab <mosl@mosl-nuc5i3.hnuphotonics.com>
2019-04-04 18:52:35 -1000

    Add dash to import.

--- finishline/finishline.py
+++ finishline/finishline.py
@@ -6,6 +6,8 @@ import traceback
 
 import finishline.grid_components as gc
 
+import dash
+
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
